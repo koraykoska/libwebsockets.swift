0d31675bc7f6352e99b2b610983e0a88cc1d2f46
Koray Koska <11356621+koraykoska@users.noreply.github.com>
2023-10-10 14:27:38 +0200

    chore: params update for efficiency

--- Sources/libwebsockets/WebsocketFrameSequence.swift
+++ Sources/libwebsockets/WebsocketFrameSequence.swift
@@ -15,25 +15,21 @@ public struct WebsocketSimpleAppendFrameSequence: WebsocketFrameSequence {
     private(set) public var binaryBuffer: Data
     private(set) public var textBuffer: Data
     public let type: WebsocketOpcode
-    private let lock: NIOLock
 
     public init(type: WebsocketOpcode) {
         self.binaryBuffer = Data()
         self.textBuffer = Data()
         self.type = type
-        self.lock = .init()
     }
 
     public mutating func append(_ frame: Data) {
-        self.lock.withLockVoid {
-            switch type {
-            case .binary:
-                self.binaryBuffer.append(frame)
-            case .text:
-                self.textBuffer.append(frame)
-            default:
-                break
-            }
+        switch type {
+        case .binary:
+            self.binaryBuffer.append(frame)
+        case .text:
+            self.textBuffer.append(frame)
+        default:
+            break
         }
     }
 }
